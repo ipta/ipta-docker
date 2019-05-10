7da2e1e1bbb4cdd9270dd9acf8f069fd84845a37
Paul T. Baker <paultbaker@gmail.com>
2019-05-10 11:03:35 -0400

    fix run script

--- ipta-gwa/run.sh
+++ ipta-gwa/run.sh
@@ -3,6 +3,7 @@
 # pull some data
 wget https://www.dropbox.com/s/yu7cfa35msa9v80/dr2cut_IPTA.tgz
 tar -xzf dr2cut_IPTA.tgz
+rm dr2cut_IPTA.tgz
 
 # use only a few pulsars for testing
 echo "J1909-3744" >> mypsrlist.txt
@@ -11,11 +12,11 @@ echo "J0437-4715" >> mypsrlist.txt
 echo "J1640+2224" >> mypsrlist.txt
 
 # run enterprise
-python gwb_analysis \
+python gwb_analysis.py \
   --psrlist mypsrlist.txt \
   --datadir partim_cut_IPTA \
   --outdir ./output/ \
   --upperlimit \
-  --Nsamp 1000 --thin 10 \
+  --Nsamp 1000 --thin 10
 
 # what else?
